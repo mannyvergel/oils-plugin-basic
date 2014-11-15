6d2390b3ec2badd0d79613142456618db8e4c01f
manny <manny@mvergel.com>
2014-11-15 18:21:31 +0800

    updated basic plugin

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-module.exports = function basicPlugin(web, next) {
+module.exports = function basicPlugin(pluginConf, web, next) {
   console.log('Hello plugin! This is a basic plugin.');
 
-  web.on('initServer', function() {
-  	console.log('Called on initialise server');
-
-  	web.get('/hello-plugin', function(req, res) {
-  		res.send('Hello Plugin');
-  	})
-  })
+  web.app.get('/hello-plugin', function(req, res) {
+      res.send('Hello Plugin');
+    })
 
   web.on('beforeRender', function(view, options, callback, req, res) {
-  	console.log('Called on before render of ' + view);
+    console.log('Called on before render of ' + view);
   })
   next();
 }
